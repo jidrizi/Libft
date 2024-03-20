f89e1b83a6eeb7bda3681877add705bcdd0ffa5a
Juxhin Idrizi <jidrizi@1-A-11.42heilbronn.de>
2024-03-20 19:51:48 +0100

    men

--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,31 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 17:03:26 by jidrizi           #+#    #+#             */
-/*   Updated: 2024/03/15 17:08:55 by jidrizi          ###   ########.fr       */
+/*   Updated: 2024/03/18 16:05:50 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char *ft_substr(char const *s, unsigned int start,
-size_t len)
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	oglen;
+	char	*substr;
+
+	oglen = ft_strlen(s);
+	if (start >= oglen)
+	{
+		return (ft_strdup(""));
+	}
+	if (start + len > oglen)
+	{
+		len = oglen - start;
+	}
+	substr = (char *)malloc(len + 1);
+	if (substr == 0)
+	{
+		return (0);
+	}
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
+}
