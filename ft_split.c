1a769acb1d848c9820432c68b077696770b417a0
Juxhin Idrizi <jidrizi@1-C-8.42heilbronn.de>
2024-03-26 20:34:17 +0100

    done?

--- ft_split.c
+++ ft_split.c
@@ -6,94 +6,79 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 19:33:09 by jidrizi           #+#    #+#             */
-/*   Updated: 2024/03/26 18:01:58 by jidrizi          ###   ########.fr       */
+/*   Updated: 2024/03/26 20:33:31 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	fill(char const *s, char c, int size, char **array)
-{
-	int	x;
-	int	y;
-	int	len;
-
-	x = 0;
-	y = 0;
-	while (size > x)
-	{
-		len = 0;
-		while (s[y] && s[y] == c)
-			y++;
-		while (s[y] && s[y] != c)
-		{
-			len++;
-			y++;
-		}
-		array[x] = malloc((len + 1) * sizeof(char));
-		if (!array[x])
-			return (x + 1);
-		ft_strlcpy(array[x], s + y - len, len + 1);
-		x++;
-	}
-	return (0);
-}
-
-static void	bruh(char **array, int bro)
-{
-	int	a;
-
-	a = 0;
-	while (a < bro)
-	{
-		free(array[a]);
-		a++;
-	}
-	free(array);
-}
-
-static size_t	counting_with_euene(char const *s, char c)
+static size_t	wcount(const char *str, char c)
 {
 	size_t	count;
 
 	count = 0;
-	if (!(*s))
-		return (0);
+	while (*str)
+	{
+		while (*str == c)
+			str++;
+		if (*str)
+			count++;
+		while (*str && *str != c)
+			str++;
+	}
+	return (count);
+}
+
+static void	freemem(char **str, int count)
+{
+	while (count >= 0)
+	{
+		free(str[count]);
+		count--;
+	}
+	free(str);
+}
+
+static void	mem_allocate(char ***array, char const *s, char c)
+{
+	char		**array_n;
+	char const	*temp;
+
+	array_n = *array;
 	while (*s)
 	{
-		while (*s == c)
+		while (*s && *s == c)
 			s++;
-		count++;
-		while (*s && *s != c)
-			s++;
+		temp = s;
+		while (*temp && *temp != c)
+			temp++;
+		if (temp > s)
+		{
+			*array_n = ft_substr(s, 0, temp - s);
+			if (!*array_n)
+			{
+				freemem(*array, array_n - *array);
+				*array = NULL;
+				return ;
+			}
+			array_n++;
+		}
+		s = temp;
 	}
-	s--;
-	if (*s == c)
-		return (count - 1);
-	return (count);
+	*array_n = 0;
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**abc;
-	size_t	size;
-	int		b;
+	int			count;
+	char		**result;
 
-	if (!s)
-		return (0);
-	size = counting_with_euene(s, c);
-	abc = malloc((size + 1) * sizeof(char *));
-	if (!(abc))
-		return (0);
-	if (size)
-	{
-		b = fill(s, c, size, abc);
-		if (b != 0)
-		{
-			bruh(abc, b);
-			return (0);
-		}
-	}
-	abc[size] = 0;
-	return (abc);
+	if (s == NULL)
+		return (NULL);
+	count = wcount(s, c);
+	result = (char **)malloc(sizeof(char *) * (count + 1));
+	if (!result)
+		return (NULL);
+	mem_allocate(&result, s, c);
+	return (result);
 }
